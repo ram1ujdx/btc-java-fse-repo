75d12d82fa39b855ef76822271593cc7dba19171
ramanujdas <ramanujds9@gmail.com>
2021-05-24 21:55:00 +0530

    Databinding

---
+++ angular-projects/btc-app/src/app/show/Person.ts
@@ -0,0 +1,7 @@
+export class Person{
+    firstName:string='';
+    lastName:string='';
+    email:string='';
+    age:number=0;
+
+}
--- angular-projects/btc-app/src/app/show/show.component.ts
+++ angular-projects/btc-app/src/app/show/show.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from './Person';
 
 @Component({
   selector: 'app-show',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowComponent implements OnInit {
 
+  
+  person:Person=new Person();
+  btnTxt:string='Click Me';
+  isDisabled:boolean=false;
   constructor() { }
 
   ngOnInit(): void {
+    // this.firstName='';
+    // this.lastName='';
+    this.btnTxt=this.isDisabled?'Disabled':'Click Me';
+
   }
+
+  // getFullName():string{
+  //   return this.firstName+' '+this.lastName;
+  // }
+
+  showInfo(){
+      alert("Person Registered..");
+  }
+
+  // clickMe(firstName:any,lastName:any){
+  //   this.firstName=firstName.value;
+  //   this.lastName=lastName.value;
+  // }
 
 }
