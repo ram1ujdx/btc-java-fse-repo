a3a48fc21edd9a7a32db1ad96a89b8f6dd66dcf8
ramanujdas <ramanujdas@192.168.10.38>
2021-07-01 21:04:26 +0530

    Updated

---
+++ spring-boot-workspace/employee-db-rest-api/src/main/java/com/btc/springboot/app/EmployeeDbRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.btc.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDbRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDbRestApiApplication.class, args);
+	}
+
+}
---
+++ spring-boot-workspace/employee-db-rest-api/src/test/java/com/btc/springboot/app/EmployeeDbRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btc.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDbRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
